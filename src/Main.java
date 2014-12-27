7d225050b5496c694a1b5c626bb5038d300b53c6
Sean Strout <sps@cs.rit.edu>
2014-12-27 08:58:37 -0500

    Added comments.  Testing github.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+/**
+ * A simple demo program using lambda's in Java 8.
+ *
+ * @author: Sean Strout (RIT CS)
+ */
+
 public class Main {
 
+    /**
+     * The main program takes the first command line argument and uses
+     * it in a print message that is spawned in a thread.
+     *
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java Main arg");
             return;
         }
 
+        // The
         Runnable r = () -> System.out.println("Hello " + args[0] + "!");
         r.run();
     }
